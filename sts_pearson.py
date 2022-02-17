2cf2de5acb9b1f098f56c474287106c0d19d598a
goldenfishome <yj234@georgetown.edu>
2022-02-16 23:03:32 -0500

    HW TODO 1,2

--- sts_pearson.py
+++ sts_pearson.py
@@ -1,6 +1,10 @@
 from scipy.stats import pearsonr
 import argparse
 from util import parse_sts
+from sts_nist import symmetrical_nist
+from nltk.translate.bleu_score import sentence_bleu
+import numpy as np
+from nltk.tokenize import word_tokenize
 
 
 def main(sts_data):
@@ -12,9 +16,60 @@ def main(sts_data):
 
     print(f"Found {len(texts)} STS pairs")
 
-    # TODO 2: Calculate each of the the metrics here for each text pair in the dataset
+    # TODO 2: Calculate each of the metrics here for each text pair in the dataset
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
+    for score_type in score_types:
+        if score_type =="NIST":
+            nist_score = [symmetrical_nist(text) for text in texts]
+        if score_type == "BLEU":
+            bleu_score = [sentence_bleu(text) for text in texts]
+        if score_type == "Word Error Rate" or "Edit Distance":
+            edit_distance_score = []
+            for text in texts:
+                source, target = text
+                source_toks = word_tokenize(source.lower())
+                target_toks = word_tokenize(target.lower())
+                n = len(source_toks)
+                m = len(target_toks)
+                D = np.zeros((n + 1, m + 1))  # create distance matrix
+
+                for i in range(n):
+                    D[i, 0] = i
+                for j in range(m):
+                    D[0, j] = j
+
+                for i in range(n):
+                    for j in range(m):
+                        if source_toks[i] == target_toks[j]:
+                            D[i + 1][j + 1] = D[i][j]
+                        else:
+                            D[i + 1][j + 1] = min(D[i][j], D[i + 1][j], D[i][j + 1]) + 1
+                edit_distance_score.append(D[n,m])
+
+        if score_type == "Longest common substring":
+            LCS_score = []
+            for text in texts:
+                source, target = text
+                source_toks = word_tokenize(source.lower())
+                target_toks = word_tokenize(target.lower())
+                n = len(source_toks)
+                m = len(target_toks)
+                D = np.zeros((n + 1, m + 1))  # create distance matrix
+
+                for i in range(n):
+                    D[i, 0] = i
+                for j in range(m):
+                    D[0, j] = j
+
+                for i in range(n):
+                    for j in range(m):
+                        if source_toks[i] == target_toks[j]:
+                            D[i + 1][j + 1] = D[i][j]
+                        else:
+                            D[i + 1][j + 1] = min(D[i][j], D[i + 1][j], D[i][j + 1]) + 1
+                LCS_score.append(D.max())
+
 
     #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
